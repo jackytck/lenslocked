78914733210a1dbbf3432a1a9837997caa5bab0a
Jacky Tang <jackytck@gmail.com>
2018-06-26 23:13:23 +0800

    :hammer: simplify view paths args

--- views/views.go
+++ views/views.go
@@ -9,13 +9,15 @@ import (
 var (
 	// LayoutDir gives the layout directory
 	LayoutDir = "views/layouts/"
+	// TemplateDir is the prefix of the views directory
+	TemplateDir = "views/"
 	// TemplateExt gives the template extention
 	TemplateExt = ".gohtml"
 )
 
 // NewView creates new view templates with default layouts.
 func NewView(layout string, files ...string) *View {
-	files = append(files, layoutFiles()...)
+	files = append(addTemplateExt(addTemplatePath(files)), layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -54,4 +56,20 @@ func layoutFiles() []string {
 		panic(err)
 	}
 	return files
+}
+
+func addTemplatePath(files []string) []string {
+	ret := make([]string, len(files))
+	for i, f := range files {
+		ret[i] = TemplateDir + f
+	}
+	return ret
+}
+
+func addTemplateExt(files []string) []string {
+	ret := make([]string, len(files))
+	for i, f := range files {
+		ret[i] = f + TemplateExt
+	}
+	return ret
 }
