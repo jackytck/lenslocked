200597c07a9c32d4c856cbb8bcef197c7c58a0bb
Jacky Tang <jackytck@gmail.com>
2018-09-11 23:32:05 +0800

    :sparkles: handle action of deleting gallery

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -112,6 +112,31 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 	g.EditView.Render(w, vd)
 }
 
+// Delete deletes the gallery by id.
+//
+// POST /galleries/:id/delete
+func (g *Galleries) Delete(w http.ResponseWriter, r *http.Request) {
+	gallery, err := g.galleryByID(w, r)
+	if err != nil {
+		return
+	}
+	user := context.User(r.Context())
+	if gallery.UserID != user.ID {
+		http.Error(w, "Gallery not found", http.StatusNotFound)
+		return
+	}
+	var vd views.Data
+	err = g.gs.Delete(gallery.ID)
+	if err != nil {
+		vd.SetAlert(err)
+		vd.Yield = gallery
+		g.EditView.Render(w, vd)
+		return
+	}
+	// TODO: redirect to index page
+	fmt.Fprintln(w, "Successfully deleted!")
+}
+
 // Create processes the gallery form when a user tries to create a new gallery.
 //
 // POST /galleries
