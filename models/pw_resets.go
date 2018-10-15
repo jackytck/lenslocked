c3403ab08cc64fad5d271ac1c32c4d1f06650e76
Jacky Tang <jackytck@gmail.com>
2018-10-15 23:40:10 +0800

    :sparkles: add password reset validator

--- models/pw_resets.go
+++ models/pw_resets.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jackytck/lenslocked/hash"
+	"github.com/jackytck/lenslocked/rand"
+	"github.com/jinzhu/gorm"
+)
 
 type pwReset struct {
 	gorm.Model
@@ -13,6 +17,46 @@ type pwResetDB interface {
 	ByToken(token string) (*pwReset, error)
 	Create(pwr *pwReset) error
 	Delete(id uint) error
+}
+
+func newPwResetValidator(db pwResetDB, hmac hash.HMAC) *pwResetValidator {
+	return &pwResetValidator{
+		pwResetDB: db,
+		hmac:      hmac,
+	}
+}
+
+type pwResetValidator struct {
+	pwResetDB
+	hmac hash.HMAC
+}
+
+func (pwrv *pwResetValidator) ByToken(token string) (*pwReset, error) {
+	pwr := pwReset{Token: token}
+	err := runPwResetValFns(&pwr, pwrv.hmacToken)
+	if err != nil {
+		return nil, err
+	}
+	return pwrv.pwResetDB.ByToken(pwr.TokenHash)
+}
+
+func (pwrv *pwResetValidator) Create(pwr *pwReset) error {
+	err := runPwResetValFns(pwr,
+		pwrv.requireUserID,
+		pwrv.setTokenIfUnset,
+		pwrv.hmacToken,
+	)
+	if err != nil {
+		return err
+	}
+	return pwrv.pwResetDB.Create(pwr)
+}
+
+func (pwrv *pwResetValidator) Delete(id uint) error {
+	if id < 0 {
+		return ErrIDInvalid
+	}
+	return pwrv.pwResetDB.Delete(id)
 }
 
 type pwResetGorm struct {
@@ -35,4 +79,42 @@ func (pwrg *pwResetGorm) Create(pwr *pwReset) error {
 func (pwrg *pwResetGorm) Delete(id uint) error {
 	pwr := pwReset{Model: gorm.Model{ID: id}}
 	return pwrg.db.Delete(&pwr).Error
+}
+
+func (pwrv *pwResetValidator) requireUserID(pwr *pwReset) error {
+	if pwr.UserID < 0 {
+		return ErrUserIDRequired
+	}
+	return nil
+}
+
+func (pwrv *pwResetValidator) setTokenIfUnset(pwr *pwReset) error {
+	if pwr.Token != "" {
+		return nil
+	}
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+	pwr.Token = token
+	return nil
+}
+
+func (pwrv *pwResetValidator) hmacToken(pwr *pwReset) error {
+	if pwr.Token == "" {
+		return nil
+	}
+	pwr.TokenHash = pwrv.hmac.Hash(pwr.Token)
+	return nil
+}
+
+type pwResetValFn func(*pwReset) error
+
+func runPwResetValFns(pwr *pwReset, fns ...pwResetValFn) error {
+	for _, fn := range fns {
+		if err := fn(pwr); err != nil {
+			return err
+		}
+	}
+	return nil
 }
